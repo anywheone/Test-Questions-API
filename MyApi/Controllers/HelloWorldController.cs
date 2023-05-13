9a0008448c9607a75a3f6d53a506637a6efec075
tatsuya imaizumi <q.3.ptatsuya@gmail.com>
2023-05-13 14:54:01 +0900

    mac-push

--- MyApi/Controllers/HelloWorldController.cs
+++ MyApi/Controllers/HelloWorldController.cs
@@ -3,13 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HelloWorldController : ControllerBase
     {
         [HttpGet]
+        [Route("get")]
         public IActionResult Get()
         {
             return Ok("Hello World!");
+        }
+
+        [HttpGet]
+        [Route("get2")]
+        public IActionResult Get2()
+        {
+            return Ok("Hello World!2");
         }
     }
 }
